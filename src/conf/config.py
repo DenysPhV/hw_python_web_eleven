b12c49b49f465504eed334ff1d8bde17f70a321e
Denys Filichkin <denys.philichkin@gmail.com>
2023-08-04 11:23:39 +0300

    easyer corrected

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,11 +1,5 @@
-from dotenv import dotenv_values
-
-
 class Config:
-    # config = dotenv_values(".env")
-    # DB_URL = config.get("DATABASE_URL")
-    # DB_URL = "postgresql://postgres:secret@127.0.0.1:5432/todo_db"
-    DB_URL = "posgresql+asyncpg://postgres:58796@localhost/todo_db"
+    DATABASE_URL = "posgresql://postgres:58796@localhost/todo_db"
 
 
 config = Config
